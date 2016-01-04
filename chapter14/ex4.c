3f9730f76b1149bcc7affaa3708225b38de1d455
tttimit <timit.cloud@gmail.com>
2016-01-04 23:02:57 +0800

    chapter14 exercise ex4.c at home

--- chapter14/ex4.c
+++ chapter14/ex4.c
@@ -7,4 +7,50 @@ a. 第一个成员是社会保障号；第二个成员是一个含有三个成员的结构。它的第一个成员
     为空，那么它的第一个字母和句点都不会输出。写一个函数来实现输出，把结构数组传
     递给这个函数。
 b. 修改a部分，传递结构的值而不是结构地址。*/
+#include <stdio.h>
+#include <string.h>
+#define LEN 20
+#define LENG 5
+struct fullname{
+	char fname[LEN];
+	char mname[LEN];
+	char lname[LEN];
+};
+struct pp{
+	char ssnum[20];
+	struct fullname name;
+};
+void print(const struct pp names[], int);
+int main(void)
+{
+	struct pp names[LENG] = {
+		{"301", {"Geogle", "Wlaker", "Bush"}},
+		{"303", {"Robin", "Yanhong", "Lee"}},
+		{"305", {"Dribble", "Michael", "Flossie"}},
+		{"310", {"Jinping","" , "Xi"}},
+		{"333", {"Edison", "Hingwa", "Chan"}}
+	};
+	
+	print(names, LENG);
 
+	return 0;
+}
+
+void print(const struct pp names[], int length)
+{
+	int i;
+	struct pp t; 
+
+	for(i = 0; i < length; i++)
+	{
+		t = names[i];
+		if(strlen(t.name.mname) == 0)
+		{
+			printf("%s, %s - %s\n", t.name.fname, t.name.lname, t.ssnum);
+		}
+		else
+		{		
+			printf("%s, %s %c. - %s\n", t.name.fname, t.name.lname, t.name.mname[0], t.ssnum);
+		}
+	}
+}
